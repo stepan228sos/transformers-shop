a99bc9bb321df1830faefe04543e6ee223600dcc
Степан <stepa.scharikov@yandex.ru>
2026-05-17 17:09:56 +0300

    1 commit

---
+++ js/script.js
@@ -0,0 +1,82 @@
+let cart = [];
+
+let products = document.querySelectorAll(".product");
+let buttons = document.querySelectorAll(".add-to-cart");
+let cartItems = document.querySelector("#cart-items");
+let total = document.querySelector("#total");
+let clearCart = document.querySelector("#clear-cart");
+let payButton = document.querySelector("#pay-btn");
+let filter = document.querySelector("#filter");
+
+const calculateTotal = () => {
+    let sum = 0;
+
+    cart.forEach(function(item) {
+        sum = sum + item.price;
+    });
+
+    return sum;
+};
+
+function showCart() {
+    let text = "";
+
+    cart.forEach(function(item, index) {
+        text = text + (index + 1) + ". " + item.name + " - " + item.price + " руб. ";
+        text = text + '<button onclick="removeFromCart(' + index + ')">Удалить</button><br>';
+    });
+
+    cartItems.innerHTML = text;
+    total.textContent = "Итого: " + calculateTotal() + " руб.";
+}
+
+function removeFromCart(index) {
+    cart.splice(index, 1);
+    showCart();
+}
+
+buttons.forEach(function(button) {
+    button.addEventListener("click", function() {
+        let product = button.parentElement;
+
+        let name = product.dataset.name;
+        let price = Number(product.dataset.price);
+
+        let item = {
+            name: name,
+            price: price
+        };
+
+        cart.push(item);
+        showCart();
+    });
+});
+
+clearCart.addEventListener("click", function() {
+    cart = [];
+    showCart();
+});
+
+payButton.addEventListener("click", function() {
+    if (cart.length === 0) {
+        alert("Корзина пуста");
+    } else {
+        alert("Покупка прошла успешно");
+        cart = [];
+        showCart();
+    }
+});
+
+filter.addEventListener("change", function() {
+    let category = filter.value;
+
+    products.forEach(function(product) {
+        if (category === "all") {
+            product.style.display = "block";
+        } else if (product.dataset.category === category) {
+            product.style.display = "block";
+        } else {
+            product.style.display = "none";
+        }
+    });
+});
